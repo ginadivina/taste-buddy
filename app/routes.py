f9edc8a8100b77d296b36901b59d45b19340838a
Georgina <georginaszanyel@gmail.com>
2019-09-20 13:34:15 +1000

    Base routes and their pages added

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,43 @@
-from flask import render_template
+from flask import render_template, url_for
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'John Smith'}
     return render_template('base.html', title='Home', user=user)
+
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+
+
+@app.route('/sign_in')
+def sign_in():
+    return render_template('sign_in.html')
+
+
+@app.route('/sign_out')
+def sign_out():
+    return render_template('sign_out.html')
+
+
+@app.route('/history')
+def history():
+    return render_template('history.html')
+
+
+@app.route('/review')
+def review():
+    return render_template('review.html')
+
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
 
 @app.errorhandler(404)
 def page_not_found(e):
